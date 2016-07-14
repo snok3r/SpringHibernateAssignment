9f3fc7073341d9e881ddf6470a666f462a4c36b8
snok3r <kos.davidenko@gmail.com>
2016-07-14 20:13:31 +0300

    - fix encoding

--- src/main/java/com/kdavidenko/springmvc/configuration/AppInitializer.java
+++ src/main/java/com/kdavidenko/springmvc/configuration/AppInitializer.java
@@ -2,8 +2,10 @@ package com.kdavidenko.springmvc.configuration;
 
 import org.springframework.web.WebApplicationInitializer;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.filter.CharacterEncodingFilter;
 import org.springframework.web.servlet.DispatcherServlet;
 
+import javax.servlet.FilterRegistration;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRegistration;
@@ -12,12 +14,18 @@ public class AppInitializer implements WebApplicationInitializer {
 
     public void onStartup(ServletContext servletContext) throws ServletException {
 
-        AnnotationConfigWebApplicationContext ctx = new AnnotationConfigWebApplicationContext();
-        ctx.register(AppConfig.class);
-        ctx.setServletContext(servletContext);
+        AnnotationConfigWebApplicationContext appContext = new AnnotationConfigWebApplicationContext();
+        appContext.register(AppConfig.class);
+        appContext.setServletContext(servletContext);
+
+        FilterRegistration.Dynamic fr = servletContext.addFilter("encodingFilter",
+                new CharacterEncodingFilter());
+        fr.setInitParameter("encoding", "UTF-8");
+        fr.setInitParameter("forceEncoding", "true");
+        fr.addMappingForUrlPatterns(null, true, "/*");
 
         ServletRegistration.Dynamic servlet = servletContext.addServlet(
-                "dispatcher", new DispatcherServlet(ctx));
+                "dispatcher", new DispatcherServlet(appContext));
 
         servlet.setLoadOnStartup(1);
         servlet.addMapping("/");
